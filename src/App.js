1470865123ad953e4ae355fb89a0a6bb69129c0f
OtacilioN <otacilio.n97@gmail.com>
2019-07-22 00:57:53 -0300

    Add language flags

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import Typist from "react-typist";
+import PTBRFlag from "./images/ptbr.svg";
+import ENFlag from "./images/en.svg";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       dividerWidth: "8px",
-      renderText: false
+      renderText: false,
+      PTBRDone: false,
+      ENDone: false
     };
   }
 
@@ -18,14 +22,61 @@ class App extends React.Component {
     );
   }
 
+  onPTBRDone = () => {
+    this.setState({ PTBRDone: true });
+  };
+
+  onENDone = () => {
+    this.setState({ ENDone: true });
+  };
+
   render() {
     return (
       <div className="App-main">
         {this.state.renderText && (
-          <div style={{ display: "flex", flex: 1, alignItems: "center" }}>
-            <Typist>
-              <span style={{ color: "white" }}>Olá, eu sou Otacilio</span>
-            </Typist>
+          <div
+            style={{
+              display: "flex",
+              flex: 1,
+              justifyContent: "center",
+              flexDirection: "column"
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flex: 1,
+                justifyContent: "center",
+                flexDirection: "column"
+              }}
+            >
+              <div>
+                <h1>
+                  {this.state.PTBRDone ? (
+                    "Olá, eu sou Otacilio"
+                  ) : (
+                    <Typist onTypingDone={this.onPTBRDone}>
+                      Olá, eu sou Otacilio
+                    </Typist>
+                  )}
+                </h1>
+              </div>
+              {(this.state.PTBRDone || this.state.ENDone) && (
+                <div>
+                  <h2>
+                    <Typist>Para começar, selecione um idioma</Typist>
+                  </h2>
+                </div>
+              )}
+            </div>
+
+            <div style={{ padding: 16 }}>
+              <img
+                src={PTBRFlag}
+                alt="Bandeira do Brasil"
+                style={{ width: 80 }}
+              />
+            </div>
           </div>
         )}
         <div
@@ -33,10 +84,49 @@ class App extends React.Component {
           className="Animated-divider"
         />
         {this.state.renderText && (
-          <div style={{ display: "flex", flex: 1, alignItems: "center" }}>
-            <Typist>
-              <span style={{ color: "white" }}>Hello, I am Otacilio</span>
-            </Typist>
+          <div
+            style={{
+              display: "flex",
+              flex: 1,
+              justifyContent: "center",
+              flexDirection: "column"
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flex: 1,
+                justifyContent: "center",
+                flexDirection: "column"
+              }}
+            >
+              <div>
+                <h1>
+                  {this.state.ENDone ? (
+                    "Hello, I am Otacilio"
+                  ) : (
+                    <Typist onTypingDone={this.onENDone}>
+                      Hello, I am Otacilio
+                    </Typist>
+                  )}
+                </h1>
+              </div>
+              {(this.state.ENDone || this.state.PTBRDone) && (
+                <div>
+                  <h2>
+                    <Typist>Select a language to start</Typist>
+                  </h2>
+                </div>
+              )}
+            </div>
+
+            <div style={{ padding: 16 }}>
+              <img
+                src={ENFlag}
+                alt="Great Britain Flag"
+                style={{ width: 80 }}
+              />
+            </div>
           </div>
         )}
       </div>
